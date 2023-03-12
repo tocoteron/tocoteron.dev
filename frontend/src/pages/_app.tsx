e0990e5f34f55ee0c272ae66b931276aa370a6f4
tocoteron <tocoteron@gmail.com>
2023-03-12 16:28:29 +0900

    embed secret message for developers

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,6 +5,8 @@ import Layout from '@/components/Layout'
 import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log('Hi, developer.')
+
   return (
     <Layout>
       <Component {...pageProps} />
